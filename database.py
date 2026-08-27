7123295c82dc4ef77a1341c50c0bddd71c47132e
Kim Dung <2311632@dlu.edu.vn>
2026-08-27 21:39:06 +0700

    Create database.py

---
+++ database.py
@@ -0,0 +1,24 @@
+import pymysql
+
+
+def get_connection():
+
+    conn = pymysql.connect(
+
+        host="mysql-97afee7-dlu-a57b.k.aivencloud.com",
+
+        port=26333,
+
+        user="avnadmin",
+
+        password="  AVNS_sJpBYdghqeVECSBW6jM",
+
+        database="quan_ly_khach_hang",
+
+        ssl={
+            "ca": "ca.pem"
+        }
+
+    )
+
+    return conn
