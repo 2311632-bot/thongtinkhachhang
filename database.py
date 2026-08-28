a796a222256d616d3dbd43428e422dd5e7a39fe5
Kim Dung <2311632@dlu.edu.vn>
2026-08-28 14:04:34 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,24 @@
 import pymysql
 
-try:
+
+def get_connection():
+
     conn = pymysql.connect(
-        host="mysql-97afee7-d1u-a57b.k.aivencloud.com",
+
+        host="mysql-97afee7-dlu-a57b.k.aivencloud.com",
+
         port=26333,
+
         user="avnadmin",
+
         password="AVNS_sJpBYdghqeVECSBW6jM",
+
         database="quan_ly_khach_hang",
-        ssl={"ca":"ca.pem"}
+
+        ssl={
+            "ca": "ca.pem"
+        }
+
     )
-    print("Connected")
-except Exception as e:
-    print(e)
+
+    return conn
