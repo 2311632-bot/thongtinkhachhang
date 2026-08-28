f382c8b319827227ae8f62fe2ff3da697e70e6ee
Kim Dung <2311632@dlu.edu.vn>
2026-08-28 15:27:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ from database import get_connection
 # =========================
 
 st.set_page_config(
-    page_title="Chăm sóc khách hàng",
+    page_title="Quản lý khách hàng tiềm năng",
     page_icon="👥",
     layout="centered"
 )
 
-st.title("👥 Chăm sóc khách hàng")
+st.title("👥 Quản lý khách hàng tiềm năng")
 
 st.write(
     "Hệ thống tự động nhận diện khách hàng mới và khách hàng quay lại."
